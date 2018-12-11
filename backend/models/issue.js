2f7f3e217a9eaf04cd4ddb8f8f00b28945299c81
Zac Bell <zchrbell7@gmail.com>
2018-12-11 10:03:19 -0700

    Initial commit

---
+++ backend/models/issue.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+let Issue = new Schema({
+    title: {
+        type: String
+    },
+    responsible: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    severity: {
+        type: String
+    },
+    status: {
+        type: String,
+        default: 'Open'
+    },
+    image: { 
+        data: Buffer, 
+        contentType: String 
+    }
+});
+
+export default mongoose.model('Issue', Issue)
